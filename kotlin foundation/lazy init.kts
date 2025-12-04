796eb360930aab347e850e98bb47e6d634abe6ee
Feihuan Peng <feihuan.peng@spreetail.com>
2025-12-03 20:30:17 -0600

    sd

---
+++ kotlin foundation/lazy init.kts
@@ -0,0 +1,59 @@
+import kotlin.reflect.KProperty
+
+private object Uninitialized
+
+class SimpleLazy<T>(private val initializer: () -> T) {
+    private var cached: Any? = Uninitialized
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        if (cached === Uninitialized) {
+            cached = initializer()
+        }
+        @Suppress("UNCHECKED_CAST")
+        return cached as T
+    }
+}
+
+fun <T> lazy(initializer: () -> T): SimpleLazy<T> = SimpleLazy(initializer)
+
+class Shop {
+
+    var isOpen = false
+
+    lateinit var manager: String
+
+//    lazy means only run when called
+    val inventory: Inventory by lazy {
+        Inventory(25, 50,10,15)
+    }
+
+    fun open(personInCharge: String) {
+        manager = personInCharge
+        isOpen = true
+    }
+
+    fun close() {
+        isOpen = false
+    }
+
+    fun checkInventory(): Inventory {
+        return inventory
+    }
+
+    data class Inventory(
+        val phones: Int,
+        val chargers: Int,
+        val headphone: Int,
+        val memoryCard: Int,
+    )
+}
+
+val phoneShop = Shop()
+
+phoneShop.isOpen // false
+phoneShop.manager
+
+phoneShop.open("Feihuan")
+phoneShop.isOpen // true
+phoneShop.manager  // feihuan
+phoneShop.checkInventory()
